29b5b864256ed98ee59353b08676460676651c44
oura-hideyoshi <tomatommy.bs@gmail.com>
2024-05-12 00:29:07 +0900

    Update package name and add author information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,12 @@ const openGraphImage = {
 export const metadata: Metadata = {
   title: 'FIND GEMS 💎',
   description: "This is online board game, Let's find some gems!",
+  icons:
+    'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>💎</text></svg>',
   openGraph: {
+    title: 'FIND GEMS 💎',
+    description: "This is online board game, Let's find some gems!",
+    type: 'website',
     images: [`/api/og?${new URLSearchParams(openGraphImage)}`],
   },
 };
